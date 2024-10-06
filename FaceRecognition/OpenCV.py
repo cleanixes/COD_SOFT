7f7f44c269ecb1f9cf5730b8515d0107d5f19108
cleanixes <harlan.dunlap@gmail.com>
2024-10-05 17:03:58 -0700

    10/5/2024

--- FaceRecognition/OpenCV.py
+++ FaceRecognition/OpenCV.py
@@ -1,8 +1,8 @@
 import cv2
 
-def draw_boundary(img, classifier, scaleFactor, minNeigh, color, text):
+def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, text):
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    feautres = classifier.detectMultiScale
+    feautres = classifier.detectMultiScale(gray_img, scaleFactor, minNeighbors)
 
 
 
